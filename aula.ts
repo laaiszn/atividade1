50218e5d5ff3db9e7975f0085d271957b27afcbb
Lais Zanqueta <laiszanqueta4@gmail.com>
2025-02-20 11:31:37 -0300

    implementação classe pessoa

---
+++ aula.ts
@@ -0,0 +1,10 @@
+let nome: string = `Laís` ;
+let idade: number = 15;
+let saudacao: string = `Boa Tarde!`;
+let tempo: string = `Ler` 
+let tempo1: string = `Filmes`
+let tempo2: string = `Correr`;
+let formacao: string = `Gestão de T.i`
+console.log (`${saudacao}`);
+console.log(`Meu nome é ${nome} Zanqueta, tenho ${idade} no meu tempo livre gosto de ${tempo}, 
+    ${tempo1} e ${tempo2}. Tenho a intenção de me formar em ${formacao}`);
