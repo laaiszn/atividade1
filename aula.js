d8be08d98459a276e2e35c7d0714acba5fbaee23
Lais <laiszanqueta4@gmail.com>
2025-03-13 15:51:54 -0300

    commitando

--- aula.js
+++ aula.js
@@ -1,9 +1,10 @@
 var nome = "La\u00EDs";
 var idade = 15;
-var saudacao = "Boa Tarde!";
+var saudacao = "Bom dia!";
 var tempo = "Ler";
 var tempo1 = "Filmes";
 var tempo2 = "Correr";
 var formacao = "Gest\u00E3o de T.i";
+var formacao2 = "Engenharia da Computa\u00E7\u00E3o";
 console.log("".concat(saudacao));
-console.log("Meu nome \u00E9 ".concat(nome, " Zanqueta, tenho ").concat(idade, " anos, no meu tempo livre gosto de ").concat(tempo, ", \n    ").concat(tempo1, " e ").concat(tempo2, ". Tenho a inten\u00E7\u00E3o de me formar em ").concat(formacao));
+console.log("Meu nome \u00E9 ".concat(nome, " Zanqueta, a minha idade \u00E9 ").concat(idade, " anos , tenho como momento de lazer ").concat(tempo, ", ver ").concat(tempo1, " \n    e o que eu mais gosto \u00E9 de ").concat(tempo2, ". Pretendo ingressar em uma universidade, at\u00E9 esse momento \n    estou tendenciosa a fazer ").concat(formacao, " minha segunda op\u00E7\u00E3o \u00E9 ").concat(formacao2));
--- aula.ts
+++ aula.ts
@@ -1,10 +1,12 @@
 let nome: string = `Laís` ;
 let idade: number = 15;
-let saudacao: string = `Boa Tarde!`;
+let saudacao: string = `Bom dia!`;
 let tempo: string = `Ler` 
 let tempo1: string = `Filmes`
 let tempo2: string = `Correr`;
 let formacao: string = `Gestão de T.i`
+let formacao2: string = `Engenharia da Computação`
 console.log (`${saudacao}`);
-console.log(`Meu nome é ${nome} Zanqueta, tenho ${idade} anos, no meu tempo livre gosto de ${tempo}, 
-    ${tempo1} e ${tempo2}. Tenho a intenção de me formar em ${formacao}`);
+console.log(`Meu nome é ${nome} Zanqueta, a minha idade é ${idade} anos , tenho como momento de lazer ${tempo}, ver ${tempo1} 
+    e o que eu mais gosto é de ${tempo2}. Pretendo ingressar em uma universidade, até esse momento 
+    estou tendenciosa a fazer ${formacao} minha segunda opção é ${formacao2}`);
